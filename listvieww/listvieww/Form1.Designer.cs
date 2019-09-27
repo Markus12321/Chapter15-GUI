ea6c9b7511f2c2ece8bb262f019be1afb4ce8d78
markus <markus1abc@gmail.com>
2019-09-27 12:48:18 +0200

    Created file explorer

--- listvieww/listvieww/Form1.Designer.cs
+++ listvieww/listvieww/Form1.Designer.cs
@@ -29,12 +29,59 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.displayLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(27, 28);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(750, 394);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "folder.png");
+            this.imageList1.Images.SetKeyName(1, "file.png");
+            // 
+            // displayLabel
+            // 
+            this.displayLabel.AutoSize = true;
+            this.displayLabel.Location = new System.Drawing.Point(27, 9);
+            this.displayLabel.Name = "displayLabel";
+            this.displayLabel.Size = new System.Drawing.Size(0, 13);
+            this.displayLabel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 447);
+            this.Controls.Add(this.displayLabel);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label displayLabel;
     }
 }
 
--- listvieww/listvieww/Form1.cs
+++ listvieww/listvieww/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,82 @@ namespace listvieww
 {
     public partial class Form1 : Form
     {
+
+        string currentDirectory = Directory.GetCurrentDirectory();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            imageList1.Images.Add(Properties.Resources.folder);
+            imageList1.Images.Add(Properties.Resources.file);
+
+            LoadFilesInDirectory(currentDirectory);
+            displayLabel.Text = currentDirectory;
+        }
+
+        public void LoadFilesInDirectory(string currentDirectoryValue)
+        {
+            try
+            {
+                listView1.Items.Clear();
+                listView1.Items.Add("Go Up One Level");
+
+                currentDirectory = currentDirectoryValue;
+                DirectoryInfo newCurrentdirectory = new DirectoryInfo(currentDirectory);
+
+                DirectoryInfo[] directoryArray = newCurrentdirectory.GetDirectories();
+                FileInfo[] fileArray = newCurrentdirectory.GetFiles();
+
+                foreach(DirectoryInfo dir in directoryArray)
+                {
+                    ListViewItem newDirectoryItem = listView1.Items.Add(dir.Name);
+
+                    newDirectoryItem.ImageIndex = 0;
+                }
+
+                foreach(FileInfo file in fileArray)
+                {
+                    ListViewItem newFileItem = listView1.Items.Add(file.Name);
+
+                    newFileItem.ImageIndex = 1;
+                }
+                
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Warning: Some files may not be visible due to permission settings", "Attention", 0, MessageBoxIcon.Warning);
+
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listView1.SelectedItems.Count != 0)
+            {
+                if (listView1.Items[0].Selected)
+                {
+                    DirectoryInfo directoryObject = new DirectoryInfo(currentDirectory);
+
+                    if(directoryObject.Parent != null)
+                    {
+                        LoadFilesInDirectory(directoryObject.Parent.FullName);
+                    }
+                }
+                else
+                {
+                    string chosen = listView1.SelectedItems[0].Text;
+
+                    if(Directory.Exists(Path.Combine(currentDirectory, chosen)))
+                    {
+                        LoadFilesInDirectory(Path.Combine(currentDirectory, chosen));
+                    }
+                }
+
+                displayLabel.Text = currentDirectory;
+            }
         }
     }
 }
